5d6c7ad068e2c6ed0553a7d1f84fced19201b1d3
Feiyang Li <me@feiyang.dev>
2021-01-28 02:04:23 -0500

    Integrated combo selection with plane API

--- Assets/Scripts/UIInteraction/MinimapInteraction.cs
+++ Assets/Scripts/UIInteraction/MinimapInteraction.cs
@@ -9,8 +9,9 @@ public class MinimapInteraction : MonoBehaviour {
     public GameObject RawImage;
     public string PlaneTag;
     public string WayPointTag;
+    public int PathLength = 3;
 
-    private Vector3 ChosenTargetPos;
+    private List<Vector3> ChosenPath;
     private GameObject ChosenPlane;
 
     private void Update(){
@@ -30,30 +31,40 @@ public class MinimapInteraction : MonoBehaviour {
             EventSystem.current.RaycastAll(pointerEventData, results);
 
             if (results.Count > 0){
-                if (results[0].gameObject.layer == LayerMask.NameToLayer("WorldUI")){
-                    Debug.Log(results[0].gameObject + results[0].worldPosition.ToString());
-                    // found raw image
-                    result = results[0];
-                    Vector3 hitRawImageLocal = RawImage.transform.InverseTransformPoint(result.worldPosition);
-                    Debug.Log("RawImageLocal" + hitRawImageLocal.ToString());
+                foreach (RaycastResult possibleResult in results)
+                {
+                    if (possibleResult.gameObject.name == "Mini Map Raw Image")
+                    {
+                        result = possibleResult;
+                        Debug.Log(result.gameObject + result.worldPosition.ToString());
+                        // found raw image
+                        Vector3 hitRawImageLocal = RawImage.transform.InverseTransformPoint(result.worldPosition);
+                        Debug.Log("RawImageLocal" + hitRawImageLocal.ToString());
 
-                    // now do a ray cast from the projection camera
-                    if (RawImage == null){
-                        Debug.Log("RawImage GameObject is null, terminating cast");
-                    }
-                    float offsetX = RawImage.GetComponent<RectTransform>().rect.width / 2;
-                    float offsetY = RawImage.GetComponent<RectTransform>().rect.height / 2;
-                    Vector3 hitRawImageLocalS = new Vector3(hitRawImageLocal.x + offsetX, hitRawImageLocal.y + offsetY, hitRawImageLocal.z);
-                    Ray rayFromProjCam = projectionCamera.ScreenPointToRay(hitRawImageLocalS); 
-                    RaycastHit hit;
-                    if (Physics.Raycast(rayFromProjCam, out hit)){
-                        GameObject obj = hit.transform.gameObject;
-                        Debug.Log("Object hit" + obj);
-                        if (obj.tag == PlaneTag) ChosenPlane = obj;
-                        else if (obj.tag == WayPointTag) ChosenTargetPos = obj.transform.position;
+                        // now do a ray cast from the projection camera
+                        if (RawImage == null)
+                        {
+                            Debug.Log("RawImage GameObject is null, terminating cast");
+                        }
+                        float offsetX = RawImage.GetComponent<RectTransform>().rect.width / 2;
+                        float offsetY = RawImage.GetComponent<RectTransform>().rect.height / 2;
+                        Vector3 hitRawImageLocalS = new Vector3(hitRawImageLocal.x + offsetX, hitRawImageLocal.y + offsetY, hitRawImageLocal.z);
+                        Ray rayFromProjCam = projectionCamera.ScreenPointToRay(hitRawImageLocalS);
+                        RaycastHit hit;
+                        if (Physics.Raycast(rayFromProjCam, out hit))
+                        {
+                            GameObject obj = hit.transform.gameObject;
+                            Debug.Log("Object hit" + obj);
+                            if (ChosenPath == null) ChosenPath = new List<Vector3>();
+                            if (obj.tag == PlaneTag) ChosenPlane = obj;
+                            else if (obj.tag == WayPointTag) ChosenPath.Add(obj.transform.position);
 
-                        if (ChosenPlane != null && ChosenTargetPos != Vector3.zero) applyChosenPathToPlane();
+                            if (ChosenPlane != null && ChosenPath.Count >= PathLength) applyChosenPathToPlane();
+                        }
+
+                        break;
                     }
+                    
                 }
             } else return;
         }
@@ -62,9 +73,12 @@ public class MinimapInteraction : MonoBehaviour {
 
     private void applyChosenPathToPlane()
     {
-        Debug.Log("Guiding plane " + ChosenPlane + " towards " + ChosenTargetPos.ToString());
+        Debug.Log("Guiding plane " + ChosenPlane);
+
+        Plane ChosenPlaneComp = ChosenPlane.GetComponent<Plane>();
+        if (ChosenPlaneComp != null) ChosenPlaneComp.followPath(new List<Vector3>(ChosenPath), false, new Vector3(0, 0, 0));
 
         ChosenPlane = null;
-        ChosenTargetPos = Vector3.zero;
+        ChosenPath.Clear();
     }
 }
