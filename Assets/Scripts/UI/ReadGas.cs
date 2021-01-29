36c5223de401511fe349f17cdc1701c822d7ee11
Feiyang Li <me@feiyang.dev>
2021-01-29 00:19:32 -0500

    Hide gas bar on parking

--- Assets/Scripts/UI/ReadGas.cs
+++ Assets/Scripts/UI/ReadGas.cs
@@ -8,11 +8,22 @@ public class ReadGas : MonoBehaviour
     public Plane mPlane;
     public PropellerMotor mPropellerMotor;
     public Slider mGasSlider;
+    public GameObject GasCanvas;
 
     // Update is called once per frame
     void Update()
     {
         mGasSlider.value = 1 - (mPlane.gasTimer / mPlane.gas_limit_time);
         mPropellerMotor.PropellerSpeedCoefficient = mGasSlider.value * 0.7f;
+
+        //mPlane.currentState = PlaneState.PARKING;
+        if (mPlane.currentState == PlaneState.PARKING)
+        {
+            GasCanvas.SetActive(false);
+        } else
+        {
+            GasCanvas.SetActive(true);
+        }
+
     }
 }
